f56ed0205d2b996d831250a526838d3702a9a205
Yu.S <u11314215@gmail.com>
2026-04-28 10:01:06 +0800

    0428_Animator

---
+++ My project_0407/Assets/Scripts/player2.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float turnSpeed = 720f;
+    
+    
+
+    Rigidbody rb;
+    bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 inputDirection = new Vector3(h, 0f, v);
+        Vector3 moveDirection = inputDirection.normalized;
+
+        if (Camera.main != null)
+        {
+            Vector3 cameraForward = Camera.main.transform.forward;
+            Vector3 cameraRight = Camera.main.transform.right;
+
+            cameraForward.y = 0f;
+            cameraRight.y = 0f;
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            moveDirection = (cameraForward * inputDirection.z + cameraRight * inputDirection.x).normalized;
+        }
+
+        Vector3 move = moveDirection * moveSpeed;
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+
+        if (moveDirection.sqrMagnitude > 0f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.fixedDeltaTime);
+        }
+
+
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
